ae89a7b8e32a95ad6df806629cb201f530f40155
batmvninarkham <paulomartin012@gmail.com>
2026-06-25 14:23:30 +0300

    stream partially done

--- test.js
+++ test.js
@@ -1,18 +1,71 @@
-async function getmaches(){
-  const match = await (await fetch ("https://streamed.pk/api/matches/live")).json();
-for (const matches of match){
-  console.log(matches.title);
-  console.log(`\n`);
-for (const source of matches.sources){
-let lsource=source.source;
-  let id =source.id;
-  console.log(`${id}`)
-const data = await (await fetch(`https://streamed.pk/api/stream/${lsource}/${id}`)).json();
-  for (dt of data){
-//console.log(dt.embedUrl)
-    console.log(JSON.stringify(data, null, 2));
+async function getcategory(){
+  const categories = await (await fetch("https://streamed.pk/api/sports")).json();
+  const array= [];
+  for (const category of categories){
+    array.push(category.id);
   }
+  return array;
 }
+async function getmatchespercategory(category){
+  const match = await(await fetch(`https://streamed.pk/api/matches/${category}`)).json();
+  return match;
 }
+async function gettodaymatches(){
+  const match = await (await fetch("https://streamed.pk/api/matches/all-today")).json();
+  return match;
 }
-getmaches();
+async function gettodayspopularmatches(){
+  const match = await (await fetch("https://streamed.pk/api/matches/all-today/popular")).json();
+  return match;
+}
+async function getmatchescategorypopular(category){
+  const match = await(await fetch(`https://streamed.pk/api/matches/${category}/popular`)).json();
+  return match;
+}
+async function getlivematches(){
+  const match = await (await fetch ("https://streamed.pk/api/matches/live")).json();
+  return match;
+}
+async function getlivepopularmatches(){
+  const match = await(await fetch ("https://streamed.pk/api/matches/live/popular")).json();
+  return match;
+async function getembedurl(data) {
+  const results = [];
+
+  for (const match of data) {
+
+    if (!match.sources?.length) {
+      continue;
+    }
+
+    const matchData = {
+      title: match.title,
+      streams: []
+    };
+
+    for (const source of match.sources) {
+
+      const streams = await (
+        await fetch(
+          `https://streamed.pk/api/stream/${source.source}/${source.id}`
+        )
+      ).json();
+
+      for (const stream of streams) {
+        matchData.streams.push({
+          streamNo: stream.streamNo,
+          viewers: stream.viewers,
+          embedUrl: stream.embedUrl
+        });
+      }
+    }
+
+    results.push(matchData);
+  }
+
+  return results;
+}
+function loadStream(embedUrl) {
+    document.getElementById('stream-player').src = embedUrl;
+}
+
