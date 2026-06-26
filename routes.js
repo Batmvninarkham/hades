91f8db54f16f45030def71ae61b1070409f93551
batmvn <paulomartin012@gmail.com>
2026-06-26 19:00:22 +0300

    good and working not prod ready but ok ig

--- routes.js
+++ routes.js
@@ -1,19 +1,20 @@
 const express= require("express");
 const app = express();
 const streamapi = require("./stream");
+
 const PORT=3000;
-app.get("/stream/live",async (req,res)=>{
+app.get("/api/stream/live",async (req,res)=>{
   const match= await streamapi.getlivematches();
   const url= await streamapi.getembedurl(match);
   res.json(url);
 });
-app.get("/stream/live/popular",async (req,res)=>{
+app.get("/api/stream/live/popular",async (req,res)=>{
   const match= await streamapi.getlivepopularmatches();
   const url = await streamapi.getembedurl(match);
   res.json(url);
 });
 //get categories and related
-app.get("/stream",async(req,res)=>{
+app.get("/api/stream",async(req,res)=>{
   const {category}=req.query;
   const {popular}= req.query;
   if (category){ 
@@ -29,17 +30,16 @@ app.get("/stream",async(req,res)=>{
   const categories = await streamapi.getcategory();
   res.json(categories);
 });
-app.get("/stream/today",async(req,res)=>{
+app.get("/api/stream/today",async(req,res)=>{
   const match= await streamapi.gettodaymatches();
   const url= await streamapi.getembedurl(match);
   res.json(url);
 });
-app.get("/stream/today/popular",async(req,res)=>{
+app.get("/api/stream/today/popular",async(req,res)=>{
   const match = await streamapi.gettodayspopularmatches();
   const url= await streamapi.getembedurl(match);
-  res.json();
+  res.json(url);
 });
-
 
 app.listen(PORT,async()=>{
 console.log(`server running on  port ${PORT}`);
